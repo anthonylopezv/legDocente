6bd3cbc03cea03fb0a234406740117974c3bcd50
Anthony Lopez <anthony.lopez@unmsm.edu.pe>
2018-06-07 16:12:21 -0500

    Configurando las credenciales de la BD localmente

--- server/config/env.js
+++ server/config/env.js
@@ -2,11 +2,11 @@
 
 const env = {
   PORT: process.env.PORT || 8089,
-  DATABASE_URL: process.env.DATABASE_URL || 'jdbc:postgresql://localhost:5432/sequelize_blog_post',
-  DATABASE_NAME: process.env.DATABASE_NAME || 'sequelize_blog_post',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/minato',
+  DATABASE_NAME: process.env.DATABASE_NAME || 'minato',
   DATABASE_HOST: process.env.DATABASE_HOST || 'localhost',
   DATABASE_USERNAME: process.env.DATABASE_USERNAME || 'postgres',
-  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || '',
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || 'postgres',
   DATABASE_PORT: process.env.DATABASE_PORT || 5432,
   DATABASE_DIALECT: process.env.DATABASE_DIALECT || 'postgres',
 
